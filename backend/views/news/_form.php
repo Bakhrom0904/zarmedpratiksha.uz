13c8f3d4e2eda754c11a7bafd38705bc549ef550
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-09-29 11:17:21 +0500

    sfaegsr

--- backend/views/news/_form.php
+++ backend/views/news/_form.php
@@ -32,9 +32,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'imageFile')->fileInput() ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+<!--    --><?//= $form->field($model, 'created_at')->textInput() ?>
+<!---->
+<!--    --><?//= $form->field($model, 'updated_at')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
