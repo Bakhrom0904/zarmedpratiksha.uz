22fade2a767e130be6207c8f89392615f0c4aad5
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-10 12:32:30 +0500

    check up test 5

--- common/widgets/views/main-banner.php
+++ common/widgets/views/main-banner.php
@@ -8,15 +8,15 @@ use yii\helpers\Url;
     <div class="banner-content">
         <div class="slider banner-slider">
             <?php foreach ($items as $item) { ?>
-                <div class="h2-slider-list" style="background-image: url(<?= $item->img ?>);">
+                <div class="h2-slider-list" style="min-height: 800px">
                     <!-- <div class="main-slide-image" style="background-image: url(<?= $item->img ?>);"></div> -->
-                    <div class="container">
+                    <div class="container" style="background-image: url(<?= $item->img ?>);">
                         <div class="slide-contain">
                             <h1 class="cl-white mt-2 wow fadeInDown"><?= $item->title ?></h1>
                             <p><?= $item->description ?></p>
                             <div class="slide-btn mt-4">
                                 <a href="<?=Url::to(['contact'])?>" class="btn bg-white text-blue"><?= Lx::t('frontend', 'Get Started') ?></a>
-                            </di v>
+                            </div>
                         </div>
                     </div>
                     <div class="overlay-banner"></div>
