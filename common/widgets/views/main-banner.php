f8fec0122ffa929e6164af2e50134dbd46e37de8
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-10 22:19:43 +0500

    efgrthy

--- common/widgets/views/main-banner.php
+++ common/widgets/views/main-banner.php
@@ -8,15 +8,14 @@ use yii\helpers\Url;
     <div class="banner-content">
         <div class="slider banner-slider">
             <?php foreach ($items as $item) { ?>
-                <div class="h2-slider-list">
-                    <div class="main-slide-image" style="background-image: url(<?= $item->img ?>);background-repeat: no-repeat; background-position:left center; background-size: 77%;"></div>
+                <div class="h2-slider-list" style="min-height: 800px">
+                    <div class="main-slide-image" style="background-image: url(<?= $item->img ?>);"></div>
                     <div class="container">
                         <div class="slide-contain">
                             <h1 class="cl-white mt-2 wow fadeInDown"><?= $item->title ?></h1>
-                            <h2 class="cl-white mt-2 wow fadeInDown"><?= $item->description ?></h2>
-                            <!-- <p><?= $item->description ?></p> -->
+                            <p><?= $item->description ?></p>
                             <div class="slide-btn mt-4">
-                                <a href="<?=Url::to(['checkup'])?>" class="btn bg-white text-blue"><?= Lx::t('frontend', 'Get Started') ?></a>
+                                <a href="<?=Url::to(['contact'])?>" class="btn bg-white text-blue"><?= Lx::t('frontend', 'Get Started') ?></a>
                             </div>
                         </div>
                     </div>
