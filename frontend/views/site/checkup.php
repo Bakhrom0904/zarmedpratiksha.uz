e2b1d5015f7aed8ddbfbdce0063456ce2a0c4b9d
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-10 21:16:02 +0500

    style csss

--- frontend/views/site/checkup.php
+++ frontend/views/site/checkup.php
@@ -3,57 +3,6 @@
             <div class="sc-title-two text-center">
                 <h2 style="color:#283779">Семейная программа Check-up  -30 %</h2>
             </div>
-            <!-- <div class="row">
-                <div class="col-lg-3 col-md-6 mb-6">
-                        <div
-                            class="service-aa p-4 d-flex align-items-center border-start bw-2 bc-green bx-shadow bg-white">
-                            <div class="sv-detail pl-4">
-                                <h3 style="color:#283779">ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
-                                <h4>Концультации</h4>
-                                <h4 style="color:#F19035">Терапевт</h4>
-                                <h4 style="color:#F19035">Гинеколог</h4>
-                                
-                        
-                            </div>
-                        </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-6">
-                        <div
-                            class="service-aa p-4 d-flex align-items-center border-start bw-2 bc-green bx-shadow bg-white">
-                            <div class="sv-detail pl-4">
-                                <h3 style="color:#283779">ПРОГРАММА ДЛЯ МУЖЧИН</h3>
-                                <h4>Концультации</h4>
-                                <h4 style="color:#F19035">Терапевт</h4>
-                                <h4 style="color:#F19035">Уролог</h4>
-                                
-                        
-                            </div>
-                        </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-6">
-                        <div
-                            class="service-aa p-4 d-flex align-items-center border-start bw-2 bc-green bx-shadow bg-white">
-                            <div class="sv-detail pl-4">
-                                <h3 style="color:#283779">ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
-                                <h4>Концультации</h4>
-                                <h4 style="color:#F19035">Педиатр</h4>
-                                <h4 style="color:#F19035">Детский невропатолог </h4>
-                            </div>
-                        </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-6">
-                        <div
-                            class="service-aa p-4 d-flex align-items-center border-start bw-2 bc-green bx-shadow bg-white">
-                            <div class="sv-detail pl-4">
-                                <h4 style="color:#283779">ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h4>
-                                <h5>Концультации</h5>
-                                <h5 style="color:#F19035">Терапевт</h5>
-                                
-                        
-                            </div>
-                        </div>
-                </div>
-            </div> -->
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                 <h3>ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
@@ -62,22 +11,21 @@
                                 <h4><i class="fas fa-plus-circle"></i> &nbsp; Гинеколог</h4>             
                 </div>
                 <div class="col-lg-3 col-md-6">
-                <h3 >ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
-                                <h4 style="color:#283779">Концультации</h4>
-                                <h4><i class="fas fa-plus-circle"></i> ЭХО КГ</h4>
-                                <h4>Гинеколог</h4>             
-                </div>
-                <div class="col-lg-3 col-md-6">
-                <h3 >ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
-                                <h4 style="color:#283779">Концультации</h4>
+                <h3 >ПРОГРАММА ДЛЯ МУЖЧИН</h3>
+                                <h4 style="color:#283779">Консультации</h4>
                                 <h4>Терапевт</h4>
-                                <h4>Гинеколог</h4>             
+                                <h4>Уролог</h4>             
                 </div>
                 <div class="col-lg-3 col-md-6">
-                <h3 >ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
-                                <h4 style="color:#283779">Концультации</h4>
-                                <h4 style="color:#F19035">Терапевт</h4>
-                                <h4 style="color:#F19035">Гинеколог</h4>             
+                <h3 >ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
+                                <h4 style="color:#283779">Консультации</h4>
+                                <h4>Педиатр</h4>
+                                <h4>Детский невропатолог </h4>             
+                </div>
+                <div class="col-lg-3 col-md-6">
+                <h3>ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h3>
+                                <h4 style="color:#283779">Консультация</h4>
+                                <h4>Терапевт</h4>            
                 </div>
             </div>
         </div>
