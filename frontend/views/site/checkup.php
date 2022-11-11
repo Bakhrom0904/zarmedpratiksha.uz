a824888823961cdae92347115f67fbcd931bcd42
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-11 07:13:29 +0500

    col-6 dsfvdg

--- frontend/views/site/checkup.php
+++ frontend/views/site/checkup.php
@@ -1,28 +1,32 @@
+<?php
+use lajax\translatemanager\helpers\Language as Lx;
+
+?>
 <section class="service" style="margin-top:130px;background-color:#F19035">
         <div class="container">
             <div class="sc-title-two text-center">
                 <h2 style="color:#283779">Семейная программа Check-up  -30 %</h2>
             </div>
             <div class="row">
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-3 col-md-6 col-6">
                 <h3>ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
-                                <h4 style="color:#283779">Концультации</h4>
-                                <h4><i class="fas fa-plus-circle"></i> &nbsp; Терапевт</h4>
-                                <h4><i class="fas fa-plus-circle"></i> &nbsp; Гинеколог</h4>             
+                                <h4 style="color:#283779">Конcультации</h4>
+                                <h4><?= Lx::t('frontend', 'Therapist') ?></h4>
+                                <h4>Гинеколог</h4>             
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-3 col-md-6 col-6">
                 <h3 >ПРОГРАММА ДЛЯ МУЖЧИН</h3>
                                 <h4 style="color:#283779">Консультации</h4>
                                 <h4>Терапевт</h4>
                                 <h4>Уролог</h4>             
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-3 col-md-6 col-6">
                 <h3 >ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
                                 <h4 style="color:#283779">Консультации</h4>
                                 <h4>Педиатр</h4>
                                 <h4>Детский невропатолог </h4>             
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-3 col-md-6 col-6">
                 <h3>ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h3>
                                 <h4 style="color:#283779">Консультация</h4>
                                 <h4>Терапевт</h4>            
