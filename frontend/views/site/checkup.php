790cb99b273eca74ba770f8e85c009dbe198363a
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-11 09:31:04 +0500

    dsbgfnh

--- frontend/views/site/checkup.php
+++ frontend/views/site/checkup.php
@@ -9,25 +9,25 @@ use lajax\translatemanager\helpers\Language as Lx;
             </div>
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-6">
-                <h3>ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
+                <h3 style="color:white">ПРОГРАММА ДЛЯ ЖЕНЩИН</h3>
                                 <h4 style="color:#283779">Конcультации</h4>
                                 <h4>Терапия</h4>
                                 <h4>Гинеколог</h4>             
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                <h3 >ПРОГРАММА ДЛЯ МУЖЧИН</h3>
+                <h3 style="color:white">ПРОГРАММА ДЛЯ МУЖЧИН</h3>
                                 <h4 style="color:#283779">Консультации</h4>
                                 <h4>Терапевт</h4>
                                 <h4>Уролог</h4>             
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                <h3 >ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
+                <h3 style="color:white">ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
                                 <h4 style="color:#283779">Консультации</h4>
                                 <h4>Педиатр</h4>
                                 <h4>Детский невропатолог </h4>             
                 </div>
                 <div class="col-lg-3 col-md-6 col-6">
-                <h3>ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h3>
+                <h3 style="color:white">ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h3>
                                 <h4 style="color:#283779">Консультация</h4>
                                 <h4>Терапевт</h4>            
                 </div>
