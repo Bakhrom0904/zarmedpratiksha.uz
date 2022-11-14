fdcbb6cd127bd5bd8f9c8b52037960aa71a97c21
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-14 17:20:17 +0500

    check up -30%

--- common/widgets/views/main-banner.php
+++ common/widgets/views/main-banner.php
@@ -12,7 +12,7 @@ use yii\helpers\Url;
                     <div class="main-slide-image" style="background-image: url(<?= $item->img ?>);"></div>
                     <div class="container">
                         <div class="slide-contain">
-                            <h1 class="cl-white mt-2 wow fadeInDown"><?= $item->title ?></h1>
+                            <h1 class="cl-white mt-2 wow fadeInDown"style="text-transform:uppercase"><?= $item->title ?><span style="color:#F19035"> -30%</span></h1>
                             <h2 class="cl-white mt-2 wow fadeInDown"><?= $item->description ?></h2>
                             <div class="slide-btn mt-4">
                                 <a href="<?=Url::to(['checkup'])?>" class="btn bg-white text-blue"><?= Lx::t('frontend', 'Get Started') ?></a>
