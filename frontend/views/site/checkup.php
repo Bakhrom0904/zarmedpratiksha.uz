f7515664db25b59e0b9627d0b646cbecf6d9733e
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-15 16:51:30 +0500

    dvfbdgho

--- frontend/views/site/checkup.php
+++ frontend/views/site/checkup.php
@@ -5,10 +5,10 @@ use lajax\translatemanager\helpers\Language as Lx;
 <section class="service" style="margin-top:130px;background-color:#F6F8FA">
         <div class="container">
             <div class="sc-title-two text-center">
-                <p style="color:#283779;text-transform:uppercase;font-weight: 600;font-size:45px"><?= Lx::t('frontend', 'CHECK-UP FAMILY PROGRAM') ?><span style="color:#F19035"> -30 %</span></p>
+                <p style="color:#283779;text-transform:uppercase;font-weight: 580;font-size:43px"><?= Lx::t('frontend', 'CHECK-UP FAMILY PROGRAM') ?><span style="color:#F19035"> -30 %</span></p>
             </div>
             <div class="row">
-                
+
                     <!-- WOMEN -->
 
                 <div class="col-lg-3 col-md-6 col-6">
