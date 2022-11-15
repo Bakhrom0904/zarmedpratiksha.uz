1169da84162f7e163d528f19c8eb6ff2ede33361
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-15 15:05:17 +0500

    checkup  working language

---
+++ frontend/views/site/test.php
@@ -0,0 +1,99 @@
+<?php
+use lajax\translatemanager\helpers\Language as Lx;
+
+?>
+<section class="service" style="margin-top:130px;background-color:#F6F8FA">
+        <div class="container">
+            <div class="sc-title-two text-center">
+                <h2 style="color:#283779;text-transform:uppercase"><?= Lx::t('frontend', 'CHECK-UP FAMILY PROGRAM') ?><span style="color:#F19035"> -30 %</span></h2>
+            </div>
+            <div class="row">
+                    <!-- WOMEN -->
+
+                <div class="col-lg-3 col-md-6 col-6">
+                <h3 style="color:#283779;text-transform:uppercase"><?= Lx::t('frontend', 'PROGRAM FOR WOMEN') ?></h3>
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Consultation') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Physician') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Gynecologist') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Cardiologic investigations') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'ECG') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Radiologic examination') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Chest X-ray') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Ultrasound examinations') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Ultrasound scan of the brest') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Ultrasound scan of the uterus and ovaries') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Ultrasound scan of the abdomen') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Laboratory and diagnostic studies') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Colposcopy') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Pathology') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'cervical Smear analysis for cytology ') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'cervical smear Analysis for microflora ') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Laboratory tests') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Complete blood count') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Triglycerides') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'urine analysis') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Urea') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Creatinine') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Glucose') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Glycated hemoglobin') ?></h4>
+
+
+                                <h3 style="color:#F19035"><span style="text-decoration:line-through;">894</span>  625 <?= Lx::t('frontend', 'sum') ?></h3>                     
+                </div>
+
+                    <!-- MEN -->
+
+                <div class="col-lg-3 col-md-6 col-6">
+                <h3 style="color:#283779;text-transform:uppercase;"><?= Lx::t('frontend', 'PROGRAM FOR MEN') ?></h3>
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Consultation') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Therapist') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Urologist') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Cardiologic investigations') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'ECG') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Radiologic examination') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Chest X-ray') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Diagnostic examination') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Colonoscopy (over 55 years old )') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Ultrasound examinations') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Ultrasound scan of the abdomen') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Ultrasound scan of the prostate gland') ?></h4>
+
+                                <h4 style="color:#F19035"><?= Lx::t('frontend', 'Laboratory tests') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Total cholesterol') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Total bilirubin') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Complete blood test urine analysis') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Urea') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Creatinine') ?></h4>
+                                <h4 style="color:#283779"><?= Lx::t('frontend', 'Glucose') ?></h4>
+
+
+                                <h3 style="color:#F19035"><span style="text-decoration:line-through;">965</span>  675 <?= Lx::t('frontend', 'sum') ?></h3>                     
+                </div>
+                <div class="col-lg-3 col-md-6 col-6">
+                <h3 style="color:#F19035;text-transform:uppercase">ПРОГРАММА ДЛЯ ДЕТЕЙ</h3>
+                                <h4 style="color:#283779">Консультации</h4>
+                                <h4 style="color:#F19035">Педиатр</h4>
+                                <h4 style="color:#F19035">Детский невропатолог </h4>
+
+                                <h3 style="color:#283779"><span style="text-decoration:line-through;">584</span>  410 <?= Lx::t('frontend', 'sum') ?></h3>             
+                </div>
+                <div class="col-lg-3 col-md-6 col-6">
+                <h3 style="color:#F19035;text-transform:uppercase">ПРОГРАММА СТАНДАРТНОЙ ДИАГНОСТИКИ</h3>
+                                <h4 style="color:#283779">Консультация</h4>
+                                <h4 style="color:#F19035">Терапевт</h4>
+
+                                <h3 style="color:#283779"><span style="text-decoration:line-through;">591</span>  413 <?= Lx::t('frontend', 'sum') ?></h3>            
+                </div>
+            </div>
+        </div>
+    </section>
