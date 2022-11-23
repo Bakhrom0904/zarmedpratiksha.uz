62ca641875424f05c45590e6b4f7d5f953881936
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-11-23 16:05:59 +0500

    add in footer maps

--- common/widgets/views/footer.php
+++ common/widgets/views/footer.php
@@ -10,6 +10,7 @@ use yii\helpers\Url;
     <div class="footer-wrap bg-blue">
         <div class="container">
             <div class="row">
+
                 <div class="col-lg-4 col-md-6 pt-md-3 pt-lg-0">
                     <div class="f-maincontent bg-white border-start bw-3 bc-green">
                         <?= Html::a('<img src="/backend/web/img/logo.svg" style="max-width: 200px" alt="image"/>', Url::to('/')) ?>
@@ -18,6 +19,7 @@ use yii\helpers\Url;
                         </p>
                     </div>
                 </div>
+
                 <div class="col-lg-3 col-md-6 ps-lg-4 pt-md-3 pt-lg-0">
                     <div class="ft-lists">
                         <h3 class="cl-white text-uppercase"><?= Lx::t('frontend', 'ZARMED PRATIKSHA') ?></h3>
@@ -38,6 +40,15 @@ use yii\helpers\Url;
                         </form>
                     </div>
                 </div>
+
+                <div class="col-lg-5 col-md-6 pt-md-3 pt-lg-0">
+                    
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3072.0486070657944!2d66.94431071570048!3d39.64862060991711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f4d19fbf4b2d8d9%3A0x7349d93e43099d11!2sZARMED%20PRATIKSHA%20Bog&#39;ishamol!5e0!3m2!1sru!2s!4v1650259779786!5m2!1sru!2s"
+                            width="100%" style="border:0; min-height: 330px" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    
+                </div>
+
                 <!-- <div class="col-lg-2 col-md-6 ps-lg-4 pt-md-3 pt-lg-0">
                     <div class="ft-lists">
                         <h3 class="cl-white"><?= Lx::t('frontend', 'Useful Links') ?></h3>
@@ -53,7 +64,7 @@ use yii\helpers\Url;
                         </ul>
                     </div>
                 </div> -->
-                <div class="col-lg-3 col-md-6 ps-lg-4 pt-md-3 pt-lg-0">
+                <!-- <div class="col-lg-3 col-md-6 ps-lg-4 pt-md-3 pt-lg-0">
                     <div class="ft-lists">
                         <h3 class="cl-white"><?= Lx::t('frontend', 'Our Departments') ?></h3>
                         <ul class="d-flex flex-column">
@@ -65,7 +76,8 @@ use yii\helpers\Url;
                             <?php } } ?>
                         </ul>
                     </div>
-                </div>
+                </div> -->
+                
             </div>
         </div>
     </div>
