d5ca2f12f5e180353ac0f33db4fe3455a8526957
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-12-27 14:30:58 +0500

    years

--- frontend/views/site/doctor/profile.php
+++ frontend/views/site/doctor/profile.php
@@ -46,16 +46,24 @@ $this->title = Lx::t('frontend', "ZARMED PRATIKSHA")." | ".Yii::t('frontend', 'O
                                 <?php
                                     if($doctor->experience==22||$doctor->experience==23||$doctor->experience==24||$doctor->experience==32||$doctor->experience==33||$doctor->experience==34)
                                     {
-                                        $y='Year';
+                                ?>
+                                    <div class="w-75">
+                                        <span><?= $doctor->experience . ' ' . Lx::t('frontend','Year') ?></span>
+                                    </div>
+                                 <?php   
                                     }
                                     else
                                     {
-                                        $y='Years';
+                                 ?>
+                                        
+                                    <div class="w-75">
+                                        <span><?= $doctor->experience . ' ' . Lx::t('frontend','Years') ?></span>
+                                    </div>
+                                 <?php       
+
                                     }
                                 ?>
-                                <div class="w-75">
-                                    <span><?= $doctor->experience . ' ' . Lx::t('frontend', $y) ?></span>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
