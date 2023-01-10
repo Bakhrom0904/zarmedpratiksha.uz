4be18f742e1e74ad1f56af6ec14a87a8371d21f5
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-01-10 18:00:33 +0500

    article delete datetime

--- frontend/views/site/article/index.php
+++ frontend/views/site/article/index.php
@@ -36,11 +36,9 @@ $this->title = Yii::t('frontend', "ZARMED PRATIKSHA")." | ". Yii::t('frontend', 
                                         <p><?= $article->trimDescription ?></p>
                                     </div>
                                 </div>
-                                <!-- <div class="article-footer">
-                                    <ul>
-                                        <li class="cl-lgrey2 pr-2"><?= $article->publishedDate ?></li>
-                                    </ul>
-                                </div> -->
+                                <div class="article-footer">
+                                    
+                                </div>
                             </div>
                         </div>
                     <?php } ?>
