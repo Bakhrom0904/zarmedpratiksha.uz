82a13d16c227d8201cacc6828c5f62dbe8b4e622
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-01-17 14:22:08 +0500

    new 2022 youtube video

--- common/widgets/views/process2.php
+++ common/widgets/views/process2.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mb-3">
-                    <iframe style="width: 95%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/3IYMeFYay7Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/9SIoOMSFm8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="col-lg-6 mb-3">
                     <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/ysK1zeIfySY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
