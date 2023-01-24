bfdf242e4144522910f869ac063a27a0435f8999
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-01-24 17:06:37 +0500

    add youtube video

--- common/widgets/views/process2.php
+++ common/widgets/views/process2.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
                     <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/9SIoOMSFm8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="col-lg-6 mb-3">
-                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/ysK1zeIfySY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/1S8R4E-vtbY" title="БЕСПЛАТНЫЕ КОНСУЛЬТАЦИИ ведущих нейрохирургов клиники "ZARMED PRATIKSHA Bog’ishamol"." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
     </div>
