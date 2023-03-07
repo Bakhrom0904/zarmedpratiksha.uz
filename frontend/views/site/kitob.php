4d4732a5633188d515c80803da8bd257d9d22f7b
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-03-07 10:01:01 +0500

    pdf embed

--- frontend/views/site/kitob.php
+++ frontend/views/site/kitob.php
@@ -1,3 +1,3 @@
 <div class="container">
-<embed src="https://zarmedpratiksha.uz/book/kitob.pdf" type="application/pdf" width="100%" height="100%" />
+<embed src="https://zarmedpratiksha.uz/book/kitob.pdf" type="application/pdf" width="100%" height="800" />
 </div>
