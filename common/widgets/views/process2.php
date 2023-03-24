8caf6c133e52ac7d037ac50424040e9b0515e4cb
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-03-24 16:31:44 +0500

    Ramadan

--- common/widgets/views/process2.php
+++ common/widgets/views/process2.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mb-3">
-            <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/GcKe7BGXZC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/_x-UvSVTIK0" title="Рамадан" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="col-lg-6 mb-3">
             <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/1ViViAotG_E" title="Безразрезное лазерное дробление камня почки мини доступом (5мм)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
--- common/widgets/views/youtube.php
+++ common/widgets/views/youtube.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mb-3">
-            <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/k5t24Xk54fg" title="&quot;ZARMED PRATIKSHA Bog&#39;ishamol&quot; klinikasi pediatri Axmedova Xakima Jo‘rayevna" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/GcKe7BGXZC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="col-lg-6 mb-3">
             <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/3IYMeFYay7Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
