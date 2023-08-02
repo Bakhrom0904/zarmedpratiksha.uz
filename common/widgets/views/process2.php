bf9b52fe9a6335f42fe5f66f36c9ddf61e0185bd
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-08-02 14:59:47 +0500

    change yotube video

--- common/widgets/views/process2.php
+++ common/widgets/views/process2.php
@@ -9,17 +9,7 @@ $til=Yii::$app->language;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mb-3">
-                <?php
-                if($til=='ru')
-                {?>
-                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/2LzyBVy2CfM" title="МРТ со скидкой в ночное время ! Без очередей и без ожидания, по записи!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <?php
-                }
-                else{?>
-                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/-C9zM0cHkB4" title="МРТ чегирмаси билан!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <?php
-                }
-                ?>
+                <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/MkDxDc_6SdY" title="A&#39;zolarni saqlash bo‘yicha jarrohlik amaliyoti: chap buyrakning laparoskopik rezektsiyasi." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div class="col-lg-6 mb-3">
                 <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/GrcFU52486c" title="Бариартрия в клинике &quot;Zarmed Pratiksha Bogoishamol&quot;. Хирург Саидов Фарход Хакимович ." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
