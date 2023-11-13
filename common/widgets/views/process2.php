922994fa160dc8af8fc7b295e38b74e2ea722690
Bakhrom0904 <bahromislomov0904@gmail.com>
2023-11-13 23:09:54 +0500

    zarmed universicty video

--- common/widgets/views/process2.php
+++ common/widgets/views/process2.php
@@ -9,7 +9,17 @@ $til=Yii::$app->language;
     <div class="container">
         <div class="row">
             <div class="col-lg-6 mb-3">
-                <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/R8QZi2U3OWk" title="umurtqa pog‘onasining turli qismlaridagi churralarni" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <?php
+                if($til=='ru')
+                {?>
+                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/XQB8v1YKac0" title="Построено здание частного университета «Зармед»" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <?php
+                }
+                else{?>
+                    <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/3iT-arrjlmM" title="“Zarmed” xususiy universiteti binosi qurildi" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <?php
+                }
+                ?>
             </div>
             <div class="col-lg-6 mb-3">
                 <iframe style="width: 100%; margin:2px auto;" height="350" src="https://www.youtube.com/embed/GrcFU52486c" title="Бариартрия в клинике &quot;Zarmed Pratiksha Bogoishamol&quot;. Хирург Саидов Фарход Хакимович ." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
