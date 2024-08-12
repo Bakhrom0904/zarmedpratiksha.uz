09c88dafc1752e2d614e3e2a921e9762dc4ddd98
Bakhrom0904 <bahromislomov0904@gmail.com>
2024-08-12 23:13:55 +0500

    google tag

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -32,6 +32,16 @@ AppAsset::register($this);
 
             gtag('config', 'G-HFNXC2089W');
             </script>
+
+                  <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-46HVGK452Z"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-46HVGK452Z');
+            </script>
     </head>
     <body class="home-2">
     <?php $this->beginBody() ?> 
