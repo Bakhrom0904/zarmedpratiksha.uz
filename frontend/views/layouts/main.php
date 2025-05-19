21abedb02d457c5fc91b04c3fe474819b6868e4f
Bakhrom0904 <bahromislomo0904@gmail.com>
2025-05-19 23:59:28 +0500

    yandex metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -41,6 +41,25 @@ AppAsset::register($this);
         </script>
         <noscript><div><img src="https://mc.yandex.ru/watch/100020076" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101784135, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+            });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/101784135" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
     </head>
     <body class="home-2">
     <?php $this->beginBody() ?> 
